dfdad47c7c407004ad40f503227e55211992c192
Okuhle_Mhambi <bukhosimhambi@gmail.com>
2021-11-16 20:31:36 +0200

    changed the database name

--- basket.js
+++ basket.js
@@ -1,26 +1,26 @@
 module.exports = function FruitBasket(pool) {
 
-    async function createBasket(fruit, qty, price) {
-        await pool.query('insert into basket (fruit_name, quantity, price) values($1, $2, $3)', [fruit, qty, price])
+    async function newBasker(fruit_name, quantity, price) {
+        await pool.query('insert into basket (fruit_name, quantity, price) values($1, $2, $3)', [fruit_name, quantity, price])
     }
 
-    async function findFruit(fruit) {
+    async function findAllFruits(fruit) {
         const fruits = await pool.query('select fruit_name, quantity, price from basket where fruit_name = $1', [fruit])
 
         return fruits.rows;
     }
 
-    async function updateFruit(fruit, quantity) {
+    async function updateFruitNumber(fruit, quantity) {
         await pool.query('update basket set quantity = quantity + $2 where fruit_name = $1', [fruit, quantity])
     }
 
-    async function totalPrice(fruit) {
+    async function showTotalPrice(fruit) {
         const total = await pool.query('select (price*quantity) as price from basket where fruit_name = $1', [fruit])
 
         return total.rows[0].price;
     }
 
-    async function totalQty(fruitType) {
+    async function showTotalQty(fruitType) {
         const qty = await pool.query('select sum(price*quantity) as total from basket where fruit_name = $1', [fruitType])
         
         return qty.rows[0].total;
